ac1d57db258c00ee397ba07a6e0bc4f36b35826e
Your Name <you@example.com>
2019-08-23 11:04:44 +0800

    个人中心查询自己发布的文章

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 # Create your views here.
+from blog.models import Article
 from .models import UserInfo
 from .forms import RegisterForm, UserInfoForm, UserInfoRegisterForm, UserForm   # 引入forms.py中定义好的表单类
 
@@ -49,3 +50,11 @@ def edit_phone(request):
             user.save()
             return redirect('account:person')     
     return render(request, 'account/edit_phone.html', locals())
+
+
+@login_required(login_url='login')
+def person_article(request):
+    article_list = Article.objects.filter(author=request.user).order_by('pub_date')
+    # print(article_list)
+    return render(request, 'account/person_article.html', locals())
+
